5207fdee667ecbfa74079868c61b8d1f20520ce2
Moriah <moriahkwashington@gmail.com>
2017-10-30 15:51:37 -0400

    First commit

---
+++ Week6Assessment2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week6Assessment2.Models;
+
+namespace Week6Assessment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            northwindEntities OrderInfo = new northwindEntities();
+            List<Order> OrderData = OrderInfo.Orders.ToList();
+
+            ViewBag.OList = OrderData;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Nothing to see!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "You're still here?";
+
+            return View();
+        }
+    }
+}
